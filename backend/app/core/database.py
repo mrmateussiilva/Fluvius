dea01a29867f39ddefccf58bab7385b0bd0dfd29
mrmateussiilva <mateuspython3010@gmail.com>
2026-05-17 00:17:33 -0300

    feat: refatoracao de database.py e injecao do psycopg2 para deploy postgresql

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -5,9 +5,10 @@ from sqlalchemy.orm import Session, declarative_base, sessionmaker
 
 from app.core.config import settings
 
-# For SQLite, we need connect_args={"check_same_thread": False}
+connect_args = {"check_same_thread": False} if settings.DATABASE_URL.startswith("sqlite") else {}
+
 engine = create_engine(
-    settings.DATABASE_URL, connect_args={"check_same_thread": False}
+    settings.DATABASE_URL, connect_args=connect_args
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
